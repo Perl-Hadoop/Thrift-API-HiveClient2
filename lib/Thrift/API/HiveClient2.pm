7d41f973ff40249f1f0c0c149f083f4f4dd5c153
David Morel <david.morel@amakuru.net>
2015-09-30 18:26:27 +0200

    Added get_columns method

--- lib/Thrift/API/HiveClient2.pm
+++ lib/Thrift/API/HiveClient2.pm
@@ -17,6 +17,28 @@ use Thrift::BufferedTransport;
 
 use Thrift::API::HiveClient2::TCLIService;
 
+# See https://msdn.microsoft.com/en-us/library/ms711683(v=vs.85).aspx
+my @odbc_coldesc_fields = qw(
+    TABLE_CAT
+    TABLE_SCHEM
+    TABLE_NAME
+    COLUMN_NAME
+    DATA_TYPE
+    TYPE_NAME
+    COLUMN_SIZE
+    BUFFER_LENGTH
+    DECIMAL_DIGITS
+    NUM_PREC_RADIX
+    NULLABLE
+    REMARKS
+    COLUMN_DEF
+    SQL_DATA_TYPE
+    SQL_DATETIME_SUB
+    CHAR_OCTET_LENGTH
+    ORDINAL_POSITION
+    IS_NULLABLE
+);
+
 # Don't use XS for now, fails initializing properly with BufferedTransport. See
 # Thrift::XS documentation.
 has use_xs => (
@@ -183,11 +205,17 @@ sub _build_session_handle {
 }
 
 has _operation => (
-    is => "rwp",
-    isa     => sub {
-        die "Operation isn't a Thrift::API::HiveClient2::TExecuteStatementResp"
-            if defined $_[0] && ( ! blessed($_[0]) || !$_[0]->isa('Thrift::API::HiveClient2::TExecuteStatementResp') ) },
-    lazy    => 1,
+    is  => "rwp",
+    isa => sub {
+        die "Operation isn't a Thrift::API::HiveClient2::T*Resp"
+            if defined $_[0]
+            && (
+            !blessed( $_[0] )
+            || (   !$_[0]->isa('Thrift::API::HiveClient2::TExecuteStatementResp')
+                && !$_[0]->isa('Thrift::API::HiveClient2::TGetColumnsResp') )
+            );
+    },
+    lazy => 1,
 );
 
 has _operation_handle => (
@@ -303,6 +331,45 @@ sub execute {
     }
 }
 
+sub get_columns {
+    my $self = shift;
+    my ( $table, $schema ) = @_;
+
+    # note that not specifying a table name would return all columns for all
+    # tables we probably don't want that, but feel free to change this
+    # behaviour. Same goes for the schema name: we probably want a default
+    # value for the schema, which is what we use here.
+    die "Unspecified table name" if !$table;
+    $schema //= "default";
+
+    $self->_cleanup_previous_operation;
+
+    my $rh = $self->_client->GetColumns(
+        Thrift::API::HiveClient2::TGetColumnsReq->new(
+            {   sessionHandle => $self->_session_handle,
+                catalogName   => undef,
+                schemaName    => $schema,
+                tableName     => $table,
+                columnName    => undef,
+                confOverlay   => {}
+            }
+        )
+    );
+    if ( $rh->{status}{errorCode} ) {
+        die __PACKAGE__ . "::execute: $rh->{status}{errorMessage}";
+    }
+    $self->_set__operation($rh);
+    $self->_set__operation_handle( $rh->{operationHandle} );
+    my $columns;
+    while ( my $res = $self->fetch($rh) ) {
+        for my $line (@$res) {
+            my $idx = 0;
+            push @$columns, { map { $_ => $line->[ $idx++ ] } @odbc_coldesc_fields };
+        }
+    }
+    return $columns;
+}
+
 sub DEMOLISH {
     my $self = shift;
 
@@ -398,6 +465,13 @@ number of rows to be retrieved if it looks like a positive integer:
 
      my $rv = $client->fetch( 10_000 );
 
+=head2 get_columns
+
+Get the columns description for a table, returned in an array of hashrefs which keys are named after the result of an
+ ODBC GetColumns call. "default" is used for the schema name is none is specified as 2nd argument.
+
+    my $columns = $client->get_columns('<table name>'[, '<schema name>']);
+
 =head1 WARNING
 
 Thrift in Perl originally did not support SASL, so authentication needed to be
@@ -445,4 +519,3 @@ So we had to change the init script for hive-server2 to make it behave, adding
 Burak Gürsoy (BURAK)
 
 =cut
-
